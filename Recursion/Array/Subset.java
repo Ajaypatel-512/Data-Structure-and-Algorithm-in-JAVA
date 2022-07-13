daf09ce3c006a486a3ad43b62370426b06f2c3dd
Ajaypatel <Ajaypatelap1504@gmail.com>
2022-07-13 09:21:09 +0530

    "Array problems with recursion"

---
+++ Recursion/Array/Subset.java
@@ -0,0 +1,35 @@
+package Recursion.Array;
+
+import java.util.ArrayList;
+import java.util.List;
+/**
+ *
+ *
+ * TIME COMPLEXITY   -  O(n * 2^n)
+ * Space Complexity  -  O(n * 2^n)
+ *
+ * **/
+public class Subset {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+        List<List<Integer>> ans = subSet(arr);
+        for (List<Integer> list: ans) {
+            System.out.println(list);
+        }
+    }
+
+    private static List<List<Integer>> subSet(int[] arr) {
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+
+        for (int num : arr) {
+            int n = outer.size();
+            for (int i = 0; i < n; i++) {
+                List<Integer> internal = new ArrayList<>(outer.get(i));
+                internal.add(num);
+                outer.add(internal);
+            }
+        }
+        return outer;
+    }
+}
