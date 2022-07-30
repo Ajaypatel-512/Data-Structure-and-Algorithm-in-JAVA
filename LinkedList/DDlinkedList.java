b0ed6bb8538649afd971db95e8b3cd8477b978c3
Ajaypatel <Ajaypatelap1504@gmail.com>
2022-07-30 10:38:02 +0530

    "Adding Doubly linkedlist"

--- LinkedList/DDlinkedList.java
+++ LinkedList/DDlinkedList.java
@@ -1,5 +1,59 @@
 package LinkedList;
 
 public class DDlinkedList {
+    Node head;
 
+    //INSERT FIRST
+    public void insertFirst(int value){
+        Node node = new Node(value);
+        node.next = head;
+        node.prev = null;
+        if(head != null) {
+            head.prev = node;
+        }
+        head = node;
+    }
+
+    public void display(){
+        Node node = head;
+        while(node != null){
+            System.out.print(node.value+" -> ");
+            node = node.next;
+        }
+        System.out.println("END");
+    }
+
+    public void displayRev(){
+        Node node = head;
+        Node last = null;
+        while(node != null){
+            System.out.print(node.value+" -> ");
+            last = node;
+            node = node.next;
+        }
+        System.out.println("END");
+        System.out.println();
+        while (last != null){
+            System.out.print(last.value +" -> ");
+            last =  last.prev;
+        }
+        System.out.println("START");
+    }
+
+
+    private class Node{
+        int value;
+        Node next;
+        Node prev;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node next, Node prev) {
+            this.value = value;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
 }
--- LinkedList/Main.java
+++ LinkedList/Main.java
@@ -2,20 +2,28 @@ package LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        LLlist ll = new LLlist();
+//        LLlist ll = new LLlist();
+//        ll.insertFirst(3);
+//        ll.insertFirst(8);
+//        ll.insertFirst(2);
+//        ll.insertFirst(28);
+//        ll.insertPos(103,3);
+//        ll.insertLast(99);
+//        ll.display();
+//        System.out.println(ll.deleteFirst());
+//        ll.display();
+//        System.out.println(ll.deleteLast());
+//        ll.display();
+//        System.out.println(ll.deletePos(2));
+//        ll.display();
+//        System.out.println(ll.find(8));
+
+
+        DDlinkedList ll = new DDlinkedList();
         ll.insertFirst(3);
         ll.insertFirst(8);
         ll.insertFirst(2);
         ll.insertFirst(28);
-        ll.insertPos(103,3);
-        ll.insertLast(99);
-        ll.display();
-        System.out.println(ll.deleteFirst());
-        ll.display();
-        System.out.println(ll.deleteLast());
-        ll.display();
-        System.out.println(ll.deletePos(2));
-        ll.display();
-        System.out.println(ll.find(8));
+        ll.displayRev();
     }
 }
