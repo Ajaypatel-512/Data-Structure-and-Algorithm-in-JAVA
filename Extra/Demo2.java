9e61671321959c6a7565802d05a9517299b5bdcf
Ajaypatel <Ajaypatelap1504@gmail.com>
2022-08-10 10:49:13 +0530

    "Extra problems"

--- Extra/Demo2.java
+++ Extra/Demo2.java
@@ -2,6 +2,14 @@ package Extra;
 
 public class Demo2 {
     public static void main(String[] args) {
-
+        System.out.println(-10%3);
+        System.out.println(-10%-3);
+        System.out.println(10%-3);
+        System.out.println(10%3);
+        System.out.println();
+        System.out.println(-10/3);
+        System.out.println(-10/-3);
+        System.out.println(10/-3);
+        System.out.println(10/3);
     }
 }
