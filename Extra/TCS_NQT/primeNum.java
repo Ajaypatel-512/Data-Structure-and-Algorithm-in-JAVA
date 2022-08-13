dab9f290236e2f9e6abd0adac72fcadb1ef55f46
Ajaypatel <Ajaypatelap1504@gmail.com>
2022-08-13 09:30:44 +0530

    "Prime number problems"

---
+++ Extra/TCS_NQT/primeNum.java
@@ -0,0 +1,32 @@
+package Extra.TCS_NQT;
+
+//Write a program to print all the prime numbers within a specific range
+
+import java.util.Scanner;
+
+public class primeNum {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the first number : ");
+        int start = s.nextInt();
+        System.out.print("Enter the second number : ");
+        int end = s.nextInt();
+        System.out.println("List of prime numbers between " + start + " and " + end);
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                System.out.print(i+" ");
+            }
+        }
+    }
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
