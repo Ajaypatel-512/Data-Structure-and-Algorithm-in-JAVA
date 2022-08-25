3dffd9ddf7b5d3fbde8b3929b4f6950e9c1f638f
Ajaypatel <Ajaypatelap1504@gmail.com>
2022-08-25 15:09:21 +0530

    "adding hashset concept"

--- Set_Interface/hashSet.java
+++ Set_Interface/hashSet.java
@@ -1,6 +1,9 @@
 package Set_Interface;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.stream.Collectors;
 // Unique values
 // unordered
 // null allowed
@@ -11,14 +14,28 @@ import java.util.HashSet;
 // add, remove, isEmpty, clear, size(), contains()
 public class hashSet {
     public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,3,5,6};
         HashSet<Integer> set = new HashSet<>();
-        set.add(19);
-        set.add(119);
-        set.add(190);
-        set.add(45);
-        set.add(96);
-        set.add(190);
-        System.out.println(set);
+//        set.add(19);
+//        set.add(119);
+//        set.add(190);
+//        set.add(45);
+//        set.add(96);
+//        set.add(190);
+//        System.out.println(set);
+
+        for (int i:arr) {
+            set.add(i);
+        }
+
+//        System.out.println(set);
+//        Iterator<Integer> i = set.iterator();
+//        while (i.hasNext()){
+//            System.out.println(i.next()+" ");
+//        }
+
+//        System.out.println(Arrays.stream(set).collect(Collectors.toSet()));
+
 
 //        for (int i:set) {
 //            System.out.println(i);
