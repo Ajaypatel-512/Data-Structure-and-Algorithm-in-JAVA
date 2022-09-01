18b444e6a502de9f184f3475cd6d9d698b965fa0
Ajaypatel <Ajaypatelap1504@gmail.com>
2022-09-01 23:29:06 +0530

    "adding arrays problem"

--- Arrayss/ArrayL1.java
+++ Arrayss/ArrayL1.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 
 public class ArrayL1 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        ArrayList<Integer> list = new ArrayList<>(2);
+//        Scanner sc = new Scanner(System.in);
+//        ArrayList<Integer> list = new ArrayList<>(2);
 //        list.add(12);
 //        list.add(100);
 //        list.add(12);
@@ -25,14 +25,40 @@ public class ArrayL1 {
 //        list.set(0,101);
 //        System.out.println(list);
 
-        for (int i = 0; i < 5; i++) {
-            list.add(sc.nextInt());
+//        for (int i = 0; i < 5; i++) {
+//            list.add(sc.nextInt());
+//        }
+//
+//        for (int i = 0; i < 5; i++) {
+//            System.out.print(list.get(i)+ " ");
+//        }
+//        System.out.println("\n"+list);
+        int n = 10;
+        System.out.println(prime(n));
+
+    }
+    static ArrayList<Integer> prime(int n){
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        if(n<=1){
+            return ans;
         }
 
-        for (int i = 0; i < 5; i++) {
-            System.out.print(list.get(i)+ " ");
+        for(int i=1; i<n; i++){
+            if(isPrime(i)) {
+                ans.add(i);
+            }
         }
-        System.out.println("\n"+list);
+        return ans;
+    }
+
+    static boolean isPrime(int n){
+        for(int i=2; i<n; i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
 
     }
 }
