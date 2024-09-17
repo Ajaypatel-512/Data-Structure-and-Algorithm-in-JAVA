cec2b2f3a3df619f4da82da622548e5d2630b245
Ajaypatel <Ajaypatelap1504@gmail.com>
2024-09-17 11:16:05 +0530

    Square Of Elements in Array then Sort approach 2

--- Arrayss/SquareOfNums.java
+++ Arrayss/SquareOfNums.java
@@ -5,9 +5,11 @@ import java.util.Arrays;
 public class SquareOfNums {
     public static void main(String[] args) {
         int[] nums = {-4,-3,-1,0,1,3,4,9};
-        System.out.println(Arrays.toString(sortedSquares(nums)));
+        System.out.println(Arrays.toString(sortedSquares2(nums)));
     }
 
+    /* APPROACH 1 - USING EXTRA SPACE
+     * */
     public static int[] sortedSquares(int[] nums) {
         int[] result = new int[nums.length];
 
@@ -18,4 +20,17 @@ public class SquareOfNums {
         Arrays.sort(result);
         return result;
     }
+
+    /* APPROACH 2 - WITHOUT USING EXTRA SPACE
+     * */
+    public static int[] sortedSquares2(int[] nums) {
+
+        for(int i=0; i<nums.length; i++){
+            nums[i] = Math.abs(nums[i] * nums[i]);
+        }
+
+        Arrays.sort(nums);
+        return nums;
+    }
+
 }
