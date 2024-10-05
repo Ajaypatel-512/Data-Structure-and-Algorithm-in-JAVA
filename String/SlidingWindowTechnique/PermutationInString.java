536e6c9daeda5cbc1f8ced3d3fd898b1d4887ca3
Ajaypatel <Ajaypatelap1504@gmail.com>
2024-10-05 11:01:41 +0530

    567. Permutation in String Approach 2 using single hash array

--- String/SlidingWindowTechnique/PermutationInString.java
+++ String/SlidingWindowTechnique/PermutationInString.java
@@ -30,7 +30,11 @@ public class PermutationInString {
      *
      * */
     public static void main(String[] args) {
+        //Approach 1
         System.out.println(checkInclusion("cab","dbabcd"));
+
+        //Approach 2
+        System.out.println(checkInclusion2("cab","dbabcd"));
     }
 
     //SOLUTION 1 - Using Sliding Window Technique O(alphabet char * s2Len)
@@ -68,4 +72,33 @@ public class PermutationInString {
         }
         return false;
     }
+
+    //SOLUTION 2 - Using Sliding Window Technique O(s1Len + s2Len).
+
+    public static boolean checkInclusion2(String s1, String s2) {
+        int len1 = s1.length(), len2 = s2.length();
+        if(len1 > len2) return false;
+
+        int count[] = new int[26];
+        for(int i=0; i<len1; i++){
+            count[s1.charAt(i)-'a']++;
+        }
+        for(int i=0; i<len2; i++){
+            count[s2.charAt(i)-'a']--;
+            if(i-len1>=0){
+                count[s2.charAt(i-len1)-'a']++;
+            }
+            if(allZero(count)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static boolean allZero(int[] count){
+        for(int i=0; i<26; i++){
+            if(count[i]!=0) return false;
+        }
+        return true;
+    }
+
 }
