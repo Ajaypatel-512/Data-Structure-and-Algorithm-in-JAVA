c8f84ee30ce9f9fb28c4b8802c5f361610938f23
Ajaypatel <Ajaypatelap1504@gmail.com>
2024-11-01 19:41:22 +0530

    1957. Delete Characters to Make Fancy String Solution 2

--- String/makeFancyString.java
+++ String/makeFancyString.java
@@ -2,11 +2,12 @@ package String;
 
 public class makeFancyString {
     public static void main(String[] args) {
-        System.out.println(makeFancyString("1aavb"));
+//        System.out.println(makeFancyString1("1aavb"));
+        System.out.println(makeFancyString2("1aavb"));
     }
 
     // Solution 1 Using Brute Force Approach O(n) O(n)
-    public static String makeFancyString(String s) {
+    public static String makeFancyString1(String s) {
         StringBuilder sb = new StringBuilder();
 
         int count = 1;
@@ -25,7 +26,26 @@ public class makeFancyString {
         return sb.toString();
     }
 
+    // Solution 1 Using Brute Force Approach O(n) O(n)
+    public static String makeFancyString2(String s) {
+        StringBuilder sb = new StringBuilder();
+        int n = s.length();
+        sb.append(s.charAt(0));
+        int freq = 1;
 
+        for (int i=1; i<n; i++){
+            if (s.charAt(i) == sb.charAt(sb.length()-1)){
+                freq++;
+                if (freq < 3){
+                    sb.append(s.charAt(i));
+                }
+            } else {
+                sb.append(s.charAt(i));
+                freq = 1;
+            }
+        }
+        return sb.toString();
+    }
 
 
 
