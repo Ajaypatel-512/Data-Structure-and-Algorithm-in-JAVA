48b08128e0d4ff9ec0e4fa8ee716fba4223527cf
Ajaypatel <Ajaypatelap1504@gmail.com>
2024-11-06 11:51:01 +0530

    3011. Find if Array Can Be Sorted Using segment sorting check

--- Arrayss/FindArrayCanBeSorted.java
+++ Arrayss/FindArrayCanBeSorted.java
@@ -3,11 +3,13 @@ package Arrayss;
 public class FindArrayCanBeSorted {
     public static void main(String[] args) {
         int[] num = {3,16,8,4,2};
-        System.out.println(canSortArray(num));
+        System.out.println(canSortArray2(num));
     }
 
 
-    //Solution 1 Using Bubble Sort O(n^2) O(1)
+    //Approach-1 (Using Bubble sorting algo)
+    //T.C : O(n^2)
+    //S.C : O(1)
     public static boolean canSortArray(int[] nums) {
         boolean swapped = true;
         for (int i=0; i<nums.length; i++){
@@ -33,4 +35,39 @@ public class FindArrayCanBeSorted {
         }
         return true;
     }
+
+
+    //Approach-2 (Using simple segment sorting check)
+    //T.C : O(n)
+    //S.C : O(1)
+    public static boolean canSortArray2(int[] nums) {
+        int n = nums.length;
+
+        int bitCount0 = Integer.bitCount(nums[0]);
+        int maxOfSegment = nums[0];
+        int minOfSegment = nums[0];
+        int maxOfPrevSegment = Integer.MIN_VALUE;
+
+        for (int i=1; i<n; i++){
+            int bitCount = Integer.bitCount(nums[i]);
+            if (bitCount0 == bitCount){
+                maxOfSegment = Math.max(maxOfSegment, nums[i]);
+                minOfSegment = Math.min(minOfSegment, nums[i]);
+            } else {
+                if (minOfSegment < maxOfPrevSegment) {
+                    return false;
+                }
+                maxOfPrevSegment = maxOfSegment;
+
+                maxOfSegment = nums[i];
+                minOfSegment = nums[i];
+                bitCount0 = Integer.bitCount(nums[i]);
+            }
+        }
+        if (minOfSegment < maxOfPrevSegment) {
+            return false;
+        }
+        return true;
+
+        }
 }
