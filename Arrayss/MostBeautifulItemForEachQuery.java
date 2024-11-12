581b3f0a446f9391e5d99854d9e5fd89c0fcc4a3
Ajaypatel <Ajaypatelap1504@gmail.com>
2024-11-12 22:30:54 +0530

    2070. Most Beautiful Item for Each Query Solution 2

--- Arrayss/MostBeautifulItemForEachQuery.java
+++ Arrayss/MostBeautifulItemForEachQuery.java
@@ -1,13 +1,14 @@
 package Arrayss;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class MostBeautifulItemForEachQuery {
     public static void main(String[] args) {
         int[][] items = {{1,2},{3,2},{2,4},{5,6},{3,5}};
         int[] queries = {1,2,3,4,5,6};
 
-        int[] result = maximumBeauty(items,queries);
+        int[] result = maximumBeauty2(items,queries);
         System.out.println(Arrays.toString(result));
     }
 
@@ -29,4 +30,50 @@ public class MostBeautifulItemForEachQuery {
         }
         return result;
     }
+
+    //Solution 2 Using Binary Search
+    //O(n) O(q)
+    public static int[] maximumBeauty2(int[][] items, int[] queries) {
+        int n = items.length;
+        int m = queries.length;
+        int[] result = new int[m];
+
+        // Step 1: Sort the items based on price in ascending order
+        Arrays.sort(items, (a, b) -> Integer.compare(a[0], b[0]));
+
+        // Update beauty values to make them non-decreasing
+        int maxBeautySeen = items[0][1];
+        for (int i = 1; i < n; i++) {
+            maxBeautySeen = Math.max(maxBeautySeen, items[i][1]);
+            items[i][1] = maxBeautySeen;
+        }
+
+        // Process each query using custom binary search
+        for (int i = 0; i < m; i++) {
+            int queryPrice = queries[i];
+            result[i] = customBinarySearch(items, queryPrice);
+        }
+
+        return result;
+    }
+
+    private static int customBinarySearch(int[][] items, int queryPrice) {
+        int l = 0;
+        int r = items.length-1;
+        int maxBeauty = 0;
+
+        while (l<=r){
+            int mid = l+ (r-l)/2;
+            if (items[mid][0] > queryPrice){
+                r = mid-1;
+            } else {
+                maxBeauty = Math.max(maxBeauty,items[mid][1]);
+                l = mid+1;
+            }
+            return maxBeauty;
+        }
+
+        return 0;
+    }
+
 }
