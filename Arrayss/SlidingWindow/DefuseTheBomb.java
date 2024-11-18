998d2aa9361a8099d46e8c7f685eb407821c9f3d
Ajaypatel <Ajaypatelap1504@gmail.com>
2024-11-18 11:17:46 +0530

    1652. Defuse the Bomb Using Sliding Window

--- Arrayss/SlidingWindow/DefuseTheBomb.java
+++ Arrayss/SlidingWindow/DefuseTheBomb.java
@@ -6,14 +6,14 @@ public class DefuseTheBomb {
     public static void main(String[] args) {
         int[] code = {5,7,1,4};
         int k = 3;
-        int[] result = decrypt(code,k);
+        int[] result = decrypt2(code,k);
         System.out.println(Arrays.toString(result));
     }
 
-    //Solution 1
+    //Solution 1 Using Brute Force
     //T.C : O(n*k)
     //S.C : O(n)
-    public static int[] decrypt(int[] code, int k) {
+    public static int[] decrypt1(int[] code, int k) {
         int n = code.length;
         int[] result = new int[n];
 
@@ -35,4 +35,38 @@ public class DefuseTheBomb {
         }
         return result;
     }
+
+    //Solution 2 Using Sliding Window
+    //T.C : O(n*k)
+    //S.C : O(n)
+    public static int[] decrypt2(int[] code, int k) {
+        int n = code.length;
+        int[] result = new int[n];
+
+        if (k == 0){
+            Arrays.fill(result,0);
+            return result;
+        }
+
+        int start = 1, end = k, sum = 0;
+
+        if (k < 0) {
+            start = n - Math.abs(k);
+            end = n - 1;
+        }
+
+        for (int i = start; i <= end; i++){
+            sum += code[i];
+        }
+
+        for (int i = 0; i < n; i++){
+            result[i] = sum;
+            sum -= code[(start) % code.length];
+            sum += code[(end + 1) % code.length];
+            start++;
+            end++;
+        }
+        return result;
+    }
+
 }
