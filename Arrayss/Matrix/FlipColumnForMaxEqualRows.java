652a0316608ddd2435c80e29a87672b00e7f1e39
Ajaypatel <Ajaypatelap1504@gmail.com>
2024-11-22 10:49:25 +0530

    1072. Flip Columns For Maximum Number of Equal Rows Using Brute Force

---
+++ Arrayss/Matrix/FlipColumnForMaxEqualRows.java
@@ -0,0 +1,81 @@
+package Arrayss.Matrix;
+
+public class FlipColumnForMaxEqualRows {
+
+    /**
+     * 1072. Flip Columns For Maximum Number of Equal Rows
+     *
+     * You are given an m x n binary matrix matrix.
+     *
+     * You can choose any number of columns in the matrix and flip every cell in that column (i.e., Change the value of the cell from 0 to 1 or vice versa).
+     *
+     * Return the maximum number of rows that have all values equal after some number of flips.
+     *
+     *
+     *
+     * Example 1:
+     *
+     * Input: matrix = [[0,1],[1,1]]
+     * Output: 1
+     * Explanation: After flipping no values, 1 row has all values equal.
+     * Example 2:
+     *
+     * Input: matrix = [[0,1],[1,0]]
+     * Output: 2
+     * Explanation: After flipping values in the first column, both rows have equal values.
+     * Example 3:
+     *
+     * Input: matrix = [[0,0,0],[0,0,1],[1,1,0]]
+     * Output: 2
+     * Explanation: After flipping values in the first two columns, the last two rows have equal values.
+     *
+     *
+     * Constraints:
+     *
+     * m == matrix.length
+     * n == matrix[i].length
+     * 1 <= m, n <= 300
+     * matrix[i][j] is either 0 or 1.
+     * */
+
+    public static void main(String[] args) {
+        int[][] matrix = {{0,1},{1,1}};
+        System.out.println(maxEqualRowsAfterFlips(matrix));
+    }
+    private static int maxi = 0;
+
+    //Solution 1 Using Brute Force
+    //T.C : O(2^m * n * m)
+    //S.C : O(n * m)
+    public static int maxEqualRowsAfterFlips(int[][] matrix) {
+        solve(0, matrix);
+        return maxi;
+    }
+
+    private static void solve(int c, int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        if (c >= m) return;
+
+        solve(c + 1, matrix);
+
+        for (int i = 0; i < n; i++) {
+            matrix[i][c] = 1 - matrix[i][c];
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            boolean flag = true;
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][j] != matrix[i][j - 1]) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) cnt++;
+        }
+
+        maxi = Math.max(maxi, cnt);
+        solve(c + 1, matrix);
+    }
+}
