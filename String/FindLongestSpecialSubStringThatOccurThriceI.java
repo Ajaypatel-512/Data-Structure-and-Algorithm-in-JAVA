53a987afaea21827c953be038664d31b5aca6271
Ajaypatel <Ajaypatelap1504@gmail.com>
2024-12-10 19:13:30 +0530

    2981. Find Longest Special Substring That Occurs Thrice I Solution 2

--- String/FindLongestSpecialSubStringThatOccurThriceI.java
+++ String/FindLongestSpecialSubStringThatOccurThriceI.java
@@ -77,4 +77,42 @@ public class FindLongestSpecialSubStringThatOccurThriceI {
         return result == 0 ? -1 : result;
     }
 
+    //Solution 2 (Better Brute Force)
+    //T.C : O(n^2)
+    //S.C : O(n^2)
+    public static int maximumLength2(String s) {
+        int n = s.length();
+        int[][] matrix = new int[26][n + 1];
+        char prevChar = s.charAt(0);
+        int length = 0;
+
+        for (int i = 0; i < n; i++) {
+            char currChar = s.charAt(i);
+
+            if (currChar == prevChar) {
+                length += 1;
+                matrix[currChar - 'a'][length] += 1;
+            } else {
+                length = 1;
+                matrix[currChar - 'a'][length] += 1;
+                prevChar = currChar;
+            }
+        }
+
+        int result = 0;
+
+        for (int i = 0; i < 26; i++) {
+            int cumSum = 0;
+            for (int j = n; j >= 1; j--) {
+                cumSum += matrix[i][j];
+                if (cumSum >= 3) {
+                    result = Math.max(result, j);
+                    break;
+                }
+            }
+        }
+
+        return result == 0 ? -1 : result;
+    }
+
 }
