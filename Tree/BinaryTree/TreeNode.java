8bc62a05704e568736d23a3570eea5057c704423
Ajaypatel <Ajaypatelap1504@gmail.com>
2024-12-20 10:46:55 +0530

    2415. Reverse Odd Levels of Binary Tree

--- Tree/BinaryTree/TreeNode.java
+++ Tree/BinaryTree/TreeNode.java
@@ -1,7 +1,12 @@
 package Tree.BinaryTree;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class TreeNode {
-    int val;
+      int val;
       TreeNode left;
       TreeNode right;
       TreeNode() {}
@@ -11,4 +16,31 @@ public class TreeNode {
           this.left = left;
           this.right = right;
       }
+
+    @Override
+    public String toString() {
+        List<String> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(this);
+
+        while (!queue.isEmpty()) {
+            TreeNode current = queue.poll();
+            if (current == null) {
+                result.add("null");
+            } else {
+                result.add(String.valueOf(current.val));
+                queue.add(current.left);
+                queue.add(current.right);
+            }
+        }
+
+        // Remove trailing "null" values for a cleaner output
+        int i = result.size() - 1;
+        while (i >= 0 && result.get(i).equals("null")) {
+            result.remove(i--);
+        }
+
+        return result.toString();
+    }
+
 }
