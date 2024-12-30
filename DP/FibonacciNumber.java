7102d4bcde07b46bbd6ede4249aa34db019ba397
Ajaypatel <Ajaypatelap1504@gmail.com>
2024-12-30 21:09:55 +0530

    509. Fibonacci Number Using Tabulation

--- DP/FibonacciNumber.java
+++ DP/FibonacciNumber.java
@@ -39,9 +39,9 @@ public class FibonacciNumber {
         System.out.println(fib(n));
     }
 
-    //Solution 1 Using Memoization
+    //Solution 1 Using Memoization (Bottom Up Approach)
     //Time Complexity: O(n)
-    //Space Complexity: O(n)
+    //Space Complexity: O(n) + O(n) = O(n) (for dp array and recursion stack)
     public static int fib(int n) {
         int dp[] = new int[n+1];
         Arrays.fill(dp,-1);
@@ -55,6 +55,19 @@ public class FibonacciNumber {
         }
         if(dp[n] != -1) return dp[n];
         dp[n] = fib(n-1) + fib(n-2);
+        return dp[n];
+    }
+
+    //Solution 2 Using Tabulation (Top Down Approach)
+    //Time Complexity: O(n)
+    //Space Complexity: O(n)
+    public static int fib2(int n) {
+        int dp[] = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
         return dp[n];
     }
 }
