374b401053ee46ada18c4463039086940555824d
Ajaypatel <Ajaypatelap1504@gmail.com>
2025-01-03 14:18:35 +0530

    2270. Number of Ways to Split Array using prefix sum without extra space

--- Arrayss/NoOfWaysToSplitArray.java
+++ Arrayss/NoOfWaysToSplitArray.java
@@ -95,5 +95,34 @@ public class NoOfWaysToSplitArray {
         return split;
     }
 
+    //Solution 3 - Prefix Sum
+    //Time Complexity - O(n)
+    //Space Complexity - O(1)
+    public static int waysToSplitArray3(int[] nums) {
+        int n = nums.length;
+
+        // Calculate the total sum of the array
+        long sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+
+        long leftSum = 0;
+        long rightSum = 0;
+        int split = 0;
+
+        // Iterate through the array to calculate leftSum and rightSum
+        for (int i = 0; i < n - 1; i++) {
+            leftSum += nums[i];
+            rightSum = sum - leftSum;
+
+            if (leftSum >= rightSum) {
+                split++;
+            }
+        }
+
+        return split;
+    }
+
 
 }
