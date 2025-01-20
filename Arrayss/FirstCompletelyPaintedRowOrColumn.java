d8e308f66b14db43a26f96d7c52772f90b34c769
Ajaypatel <Ajaypatelap1504@gmail.com>
2025-01-20 12:18:55 +0530

    2661. First Completely Painted Row or Column Using optimal approach

--- Arrayss/FirstCompletelyPaintedRowOrColumn.java
+++ Arrayss/FirstCompletelyPaintedRowOrColumn.java
@@ -127,4 +127,48 @@ public class FirstCompletelyPaintedRowOrColumn {
             }
             return -1;
         }
+
+    // Approach - 3 (Best Approach)
+    // T.C : O(m*n)
+    // S.C : O(m*n)
+    public static int firstCompleteIndex3(int[] arr, int[][] mat) {
+            int m = mat.length;
+            int n = mat[0].length;
+
+            Map<Integer, Integer> mp = new HashMap<>();
+
+            for (int i = 0; i < arr.length; i++) {
+                mp.put(arr[i], i);
+            }
+
+            int minIndex = Integer.MAX_VALUE;
+
+            // Check each row one by one
+            for (int row = 0; row < m; row++) {
+                int lastIndex = Integer.MIN_VALUE; // last index in arr when this row will be completely painted
+
+                for (int col = 0; col < n; col++) {
+                    int val = mat[row][col];
+                    int idx = mp.get(val);
+                    lastIndex = Math.max(lastIndex, idx);
+                }
+
+                minIndex = Math.min(minIndex, lastIndex);
+            }
+
+            // Check each column one by one
+            for (int col = 0; col < n; col++) {
+                int lastIndex = Integer.MIN_VALUE; // last index in arr when this col will be completely painted
+
+                for (int row = 0; row < m; row++) {
+                    int val = mat[row][col];
+                    int idx = mp.get(val);
+                    lastIndex = Math.max(lastIndex, idx);
+                }
+
+                minIndex = Math.min(minIndex, lastIndex);
+            }
+
+            return minIndex;
+        }
 }
