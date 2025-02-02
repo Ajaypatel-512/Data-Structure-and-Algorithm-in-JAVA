21bebdcae006231e941559c53dc5f977d626a32d
Ajaypatel <Ajaypatelap1504@gmail.com>
2025-02-02 18:53:06 +0530

    1752. Check if Array Is Sorted and Rotated Better and efficient approach

--- Arrayss/CheckIfArrayIsSortedAndRotated.java
+++ Arrayss/CheckIfArrayIsSortedAndRotated.java
@@ -69,4 +69,46 @@ public class CheckIfArrayIsSortedAndRotated {
     }
 
 
+    //Solution 2 : Efficient
+    //Time complexity: O(n^2)
+    //Space complexity: O(1)
+    public static boolean check2(int[] nums) {
+        int n = nums.length;
+
+        int[] sorted = nums.clone();
+        Arrays.sort(sorted);
+
+        for (int r = 0; r < n; r++) {
+            boolean isSorted = true;
+            for (int i = 0; i < n; i++) {
+                if (sorted[i] != nums[(i + r) % n]) {
+                    isSorted = false;
+                    break;
+                }
+            }
+
+            if (isSorted) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    //Solution 3 : Efficient
+    //Time complexity: O(n)
+    //Space complexity: O(1)
+    public static boolean check3(int[] nums) {
+        int n = nums.length;
+
+        int peak = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                peak++;
+            }
+        }
+
+        return peak <= 1;
+    }
+
 }
