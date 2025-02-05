afcda3698216a552585cd1dad9deeb1be8e853fd
Ajaypatel <Ajaypatelap1504@gmail.com>
2025-02-05 13:34:56 +0530

    1790. Check if One String Swap Can Make Strings Equal

---
+++ Arrayss/CheckIfOneStringSwapCanMakeStringsEqual.java
@@ -0,0 +1,62 @@
+package Arrayss;
+
+import java.util.Arrays;
+
+public class CheckIfOneStringSwapCanMakeStringsEqual {
+    /**
+     * 1790. Check if One String Swap Can Make Strings Equal
+     * You are given two strings s1 and s2 of equal length. A string swap is an operation where you choose two indices in a string (not necessarily different) and swap the characters at these indices.
+     *
+     * Return true if it is possible to make both strings equal by performing at most one string swap on exactly one of the strings. Otherwise, return false.
+     *
+     *
+     *
+     * Example 1:
+     *
+     * Input: s1 = "bank", s2 = "kanb"
+     * Output: true
+     * Explanation: For example, swap the first character with the last character of s2 to make "bank".
+     * Example 2:
+     *
+     * Input: s1 = "attack", s2 = "defend"
+     * Output: false
+     * Explanation: It is impossible to make them equal with one string swap.
+     * Example 3:
+     *
+     * Input: s1 = "kelb", s2 = "kelb"
+     * Output: true
+     * Explanation: The two strings are already equal, so no string swap operation is required.
+     *
+     *
+     * Constraints:
+     *
+     * 1 <= s1.length, s2.length <= 100
+     * s1.length == s2.length
+     * s1 and s2 consist of only lowercase English letters.
+     * */
+    public static void main(String[] args) {
+        String s1 = "bank", s2 = "kanb";
+        System.out.println(areAlmostEqual(s1, s2));
+    }
+
+    //Solution 1 - Using Brute Force
+    //Time Complexity - O(n^2)
+    //Space Complexity - O(n)
+    public static boolean areAlmostEqual(String s1, String s2) {
+        if (s1.equals(s2)) return true;
+
+        int l1 = s1.length();
+        for(int i=0; i<l1; i++){
+            for(int j=i+1; j<l1; j++) {
+                char[] s1Arr = s1.toCharArray();
+
+                char temp = s1Arr[i];
+                s1Arr[i] = s1Arr[j];
+                s1Arr[j] = temp;
+
+                if (String.valueOf(s1Arr).equals(s2)) return true;
+            }
+        }
+        return false;
+    }
+}
