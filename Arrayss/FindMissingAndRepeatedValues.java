f1db7dd9ec2c377848fab7dc1b3098753a3778f7
Ajaypatel <Ajaypatelap1504@gmail.com>
2025-03-06 11:03:46 +0530

    2965. Find Missing and Repeated Values using mathematical formula

--- Arrayss/FindMissingAndRepeatedValues.java
+++ Arrayss/FindMissingAndRepeatedValues.java
@@ -71,4 +71,34 @@ public class FindMissingAndRepeatedValues {
 
         return new int[]{a, b};
     }
+
+    //Solution 2 - Using Mathematical Formula
+    //Time Complexity - O(n^2)
+    //Space Complexity - O(1)
+    public static int[] findMissingAndRepeatedValues2(int[][] grid) {
+        int n = grid.length;
+
+        long N = (long) n * n;
+
+        long gridSum = 0;
+        long gridSqSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                gridSum += grid[i][j];
+                gridSqSum += (long) grid[i][j] * grid[i][j];
+            }
+        }
+
+        long sum = (N * (N + 1)) / 2; // Expected sum of N natural numbers
+        long sqSum = (N * (N + 1) * (2 * N + 1)) / 6; // Expected sq sum of natural numbers
+
+        long sqDiff = gridSqSum - sqSum;
+        long sumDiff = gridSum - sum;
+
+        int a = (int) ((sqDiff / sumDiff + sumDiff) / 2);
+        int b = (int) ((sqDiff / sumDiff - sumDiff) / 2);
+
+        return new int[]{a, b};
+    }
 }
