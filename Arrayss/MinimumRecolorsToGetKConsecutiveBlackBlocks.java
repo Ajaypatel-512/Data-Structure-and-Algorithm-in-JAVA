549c1f77fd607eb9de75a01b1bf9194f2ba4ff2d
Ajaypatel <Ajaypatelap1504@gmail.com>
2025-03-09 09:19:58 +0530

    2379. Minimum Recolors to Get K Consecutive Black Blocks Using Sliding Window

--- Arrayss/MinimumRecolorsToGetKConsecutiveBlackBlocks.java
+++ Arrayss/MinimumRecolorsToGetKConsecutiveBlackBlocks.java
@@ -64,4 +64,34 @@ public class MinimumRecolorsToGetKConsecutiveBlackBlocks {
         return result;
     }
 
+    //Solution 2 - Sliding Window
+    //Time Complexity - O(n)
+    //Space Complexity - O(1)
+    public static int minimumRecolors2(String blocks, int k) {
+        int n = blocks.length();
+        int i = 0;
+        int j = 0;
+
+        int ops = k;
+        int W = 0;
+
+        while (j < n) {
+            if (blocks.charAt(j) == 'W') {
+                W++;
+            }
+
+            if (j - i + 1 == k) {
+                ops = Math.min(ops, W);
+
+                if (blocks.charAt(i) == 'W') {
+                    W--;
+                }
+                i++;
+            }
+            j++;
+        }
+
+        return ops;
+    }
+
 }
