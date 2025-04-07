ff4dd59867d072160d51731bf9275567a4db4e80
Ajaypatel <Ajaypatelap1504@gmail.com>
2025-04-07 16:04:36 +0530

    Selection sort Inorder

--- Sorting/selectionSort.java
+++ Sorting/selectionSort.java
@@ -43,4 +43,23 @@ public class selectionSort {
         }
         return max;
     }
+
+
+    //Time complexity - O(N^2)
+    //Space complexity - O(1)
+    public static void Selection2(int[] arr){
+        int n = arr.length;
+        for (int i=0; i<=n-2; i++){
+            int min = i;
+            for (int j=i; j<=n-1; j++){
+                if(arr[j]<arr[min]){
+                    min = j;
+                }
+            }
+
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
 }
