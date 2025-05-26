e358d07649a3a730787c3a805d45ba8c0dd01283
Ajaypatel <Ajaypatelap1504@gmail.com>
2025-05-26 10:03:42 +0530

    Rotate Array Right By D elements

--- Arrayss/RotateRightByDElement.java
+++ Arrayss/RotateRightByDElement.java
@@ -1,30 +1,28 @@
 package Arrayss;
 
-import java.util.Arrays;
-
-public class rotateLeftDEle {
+public class RotateRightByDElement {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7};
-        int d = 1;
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int d = 2; // Number of positions to rotate
         int n = arr.length;
-        rotate(arr,n,d);
-        System.out.println(Arrays.toString(arr));
+        rotate(arr, n, d);
+        System.out.println(java.util.Arrays.toString(arr));
     }
 
     //Solution 1: Brute force
     //Time Complexity: O(N*D) where N is the size of the array and D is the number of rotations
     // Space Complexity: O(1)
-    static void leftRotate(int[] arr, int n){
-        int temp = arr[0];
-        for (int i = 1; i < n; i++) {
-            arr[i-1] = arr[i];
+    static void rightRotate(int[] arr, int n){
+        int temp = arr[n - 1];
+        for (int i = n-1; i > 0; i--) {
+            arr[i] = arr[i-1];
         }
-        arr[n-1] = temp;
+        arr[0] = temp;
     }
 
     private static void rotate1(int[] arr,int n,int d){
         for(int i=0; i<d; i++)
-            leftRotate(arr,n);
+            rightRotate(arr,n);
     }
 
     //Solution 2: Better approach
@@ -33,15 +31,15 @@ public class rotateLeftDEle {
     private static void rotate2(int[] arr,int n,int d){
         int[] temp = new int[d];
         for (int i = 0; i < d; i++) {
-            temp[i] = arr[i];
+            temp[i] = arr[n-d+i];
         }
 
         for (int i = d; i < n; i++) {
-            arr[i-d] = arr[i];
+            arr[i] = arr[i-d];
         }
 
         for (int i = 0; i < d; i++) {
-            arr[n-d+i] = temp[i];
+            arr[i] = temp[i];
         }
     }
 
@@ -50,8 +48,8 @@ public class rotateLeftDEle {
     //Time Complexity: O(N) where N is the size of the array
     // Space Complexity: O(1)
     static void rotate(int[] arr,int n,int d){
-        reverse(arr,0,d-1);
-        reverse(arr,d,n-1);
+        reverse(arr,0,n-d-1);
+        reverse(arr,n-d,n-1);
         reverse(arr,0,n-1);
     }
     static void reverse(int[] arr, int l, int h){
@@ -63,5 +61,4 @@ public class rotateLeftDEle {
             h--;
         }
     }
-
 }
