ebe6d74f03273bd36e380277be77ffd7682d3353
Ajaypatel <Ajaypatelap1504@gmail.com>
2025-05-26 09:55:36 +0530

    Rotate Array Left By D elements

--- Arrayss/rotateLeftDEle.java
+++ Arrayss/rotateLeftDEle.java
@@ -11,38 +11,44 @@ public class rotateLeftDEle {
         System.out.println(Arrays.toString(arr));
     }
 
-//    NAIVE APPROACH  O(N*D) O(1)
-//    static void leftRotate(int[] arr, int n){
-//        int temp = arr[0];
-//        for (int i = 1; i < n; i++) {
-//            arr[i-1] = arr[i];
-//        }
-//        arr[n-1] = temp;
-//    }
-//
-//    static void rotate(int[] arr,int n,int d){
-//        for(int i=0; i<d; i++)
-//            leftRotate(arr,n);
-//    }
+    //Solution 1: Brute force
+    //Time Complexity: O(N*D) where N is the size of the array and D is the number of rotations
+    // Space Complexity: O(1)
+    static void leftRotate(int[] arr, int n){
+        int temp = arr[0];
+        for (int i = 1; i < n; i++) {
+            arr[i-1] = arr[i];
+        }
+        arr[n-1] = temp;
+    }
 
-//    BETTER APPROACH  O(N) O(D)
-//    static void rotate(int[] arr,int n,int d){
-//        int[] temp = new int[d];
-//        for (int i = 0; i < d; i++) {
-//            temp[i] = arr[i];
-//        }
-//
-//        for (int i = d; i < n; i++) {
-//            arr[i-d] = arr[i];
-//        }
-//
-//        for (int i = 0; i < d; i++) {
-//            arr[n-d+i] = temp[i];
-//        }
-//    }
+    static void rotate1(int[] arr,int n,int d){
+        for(int i=0; i<d; i++)
+            leftRotate(arr,n);
+    }
+
+    //Solution 3: Better approach
+    //Time Complexity: O(N*D) where N is the size of the array and D is the number of rotations
+    // Space Complexity: O(D)
+    static void rotate2(int[] arr,int n,int d){
+        int[] temp = new int[d];
+        for (int i = 0; i < d; i++) {
+            temp[i] = arr[i];
+        }
+
+        for (int i = d; i < n; i++) {
+            arr[i-d] = arr[i];
+        }
+
+        for (int i = 0; i < d; i++) {
+            arr[n-d+i] = temp[i];
+        }
+    }
 
 
-    //    EFFICEIENT APPROACH  O(N) O(1)
+    //Solution 3: Optimal approach
+    //Time Complexity: O(N) where N is the size of the array
+    // Space Complexity: O(1)
     static void rotate(int[] arr,int n,int d){
         reverse(arr,0,d-1);
         reverse(arr,d,n-1);
