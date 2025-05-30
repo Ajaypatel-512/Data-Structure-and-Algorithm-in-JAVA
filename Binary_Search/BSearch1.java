a3691a4f4da292ee168c4a78a1d7513fa297f779
Ajaypatel <Ajaypatelap1504@gmail.com>
2025-05-30 10:55:30 +0530

    704. Binary Search using recursion

--- Binary_Search/BSearch1.java
+++ Binary_Search/BSearch1.java
@@ -1,17 +1,45 @@
 package Binary_Search;
 
 public class BSearch1 {
+    /**
+     * 704. Binary Search
+     * Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+     *
+     * You must write an algorithm with O(log n) runtime complexity.
+     * Example 1:
+     *
+     * Input: nums = [-1,0,3,5,9,12], target = 9
+     * Output: 4
+     * Explanation: 9 exists in nums and its index is 4
+     * Example 2:
+     *
+     * Input: nums = [-1,0,3,5,9,12], target = 2
+     * Output: -1
+     * Explanation: 2 does not exist in nums so return -1
+     *
+     *
+     * Constraints:
+     *
+     * 1 <= nums.length <= 104
+     * -104 < nums[i], target < 104
+     * All the integers in nums are unique.
+     * nums is sorted in ascending order.
+     * */
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7,8,9,10};
         int ans = binarysearch(arr,10);
+        int result = search(arr,10);
         System.out.println(ans);
     }
+
+    //Solution 1: Using Binary Search
+    //Time Complexity: O(log n)
+    // Space Complexity: O(1)
     public static int binarysearch(int[] arr, int target){
         int start = 0;
         int end = arr.length-1;
         while (start<=end){
 
-//        int mid = (start+end)/2;
             int mid = start + (end-start)/2;
             if(target < arr[mid]) {
                 end = mid - 1;
@@ -23,5 +51,27 @@ public class BSearch1 {
             }
         }
         return -1;
+    }
+
+    //Solution 2: Using Recursion
+    //Time Complexity: O(log n)
+    // Space Complexity: O(log n) due to recursion stack space
+
+    public static int search(int[] nums, int target) {
+        return binarysearch2(nums, 0, nums.length-1, target);
+    }
+
+    public static int binarysearch2(int[] arr,int low, int high, int target){
+        if (low > high) {
+            return -1;
+        }
+        int mid = low + (high - low) / 2;
+        if (target < arr[mid]) {
+            return binarysearch2(arr, low, mid - 1, target);
+        } else if (target > arr[mid]) {
+            return binarysearch2(arr, mid + 1, high, target);
+        } else {
+            return mid;
+        }
     }
 }
