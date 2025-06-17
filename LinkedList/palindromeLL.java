bb9eb5e42af08e79c809312034924e4e2b0f391b
Ajaypatel <Ajaypatelap1504@gmail.com>
2025-06-17 10:13:20 +0530

    234. Palindrome Linked List Using Stack

--- LinkedList/palindromeLL.java
+++ LinkedList/palindromeLL.java
@@ -1,6 +1,31 @@
 package LinkedList;
 
+import java.util.Stack;
+
 public class palindromeLL {
+    /**
+     * 234. Palindrome Linked List
+     * Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+     * Example 1:
+     *
+     *
+     * Input: head = [1,2,2,1]
+     * Output: true
+     * Example 2:
+     *
+     *
+     * Input: head = [1,2]
+     * Output: false
+     *
+     *
+     * Constraints:
+     *
+     * The number of nodes in the list is in the range [1, 105].
+     * 0 <= Node.val <= 9
+     *
+     *
+     * Follow up: Could you do it in O(n) time and O(1) space?
+     * */
     public class ListNode {
         int val;
         ListNode next;
@@ -52,6 +77,30 @@ public class palindromeLL {
         }
         reverse(reReverseHead);
         return head == null || headSec == null;
+    }
+
+    //Solution 1: Using Stack
+    //Time Complexity: O(2n)
+    //Space Complexity: O(n)
+    public static boolean isPalindrome2(ListNode head) {
+        Stack<Integer> st = new Stack<>();
+        ListNode temp = head;
+
+        while (temp != null) {
+            st.push(temp.val);
+            temp = temp.next;
+        }
+
+        temp = head;
+
+        while (temp != null) {
+            if (temp.val != st.pop()) {
+                return false;
+            }
+            temp = temp.next;
+        }
+
+        return true;
     }
 
 
