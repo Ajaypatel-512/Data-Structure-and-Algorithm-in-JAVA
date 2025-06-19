00da88485cb4bc2475cb947b1da9fdc5c9f63a47
Ajaypatel <Ajaypatelap1504@gmail.com>
2025-06-19 09:12:28 +0530

    50. Pow(x, n) using different approaches

--- Recursion/PowXN.java
+++ Recursion/PowXN.java
@@ -41,7 +41,7 @@ public class PowXN {
     // Space Complexity: O(1)
     public static double myPow(double x, int n) {
         double result = 1.0;
-        long long nn = n; // Handle negative powers by converting n to long
+        long nn = n; // Handle negative powers by converting n to long
         if (nn < 0) nn = -1 * nn;
         while (nn > 0) {
             if (nn % 2 == 1) {
