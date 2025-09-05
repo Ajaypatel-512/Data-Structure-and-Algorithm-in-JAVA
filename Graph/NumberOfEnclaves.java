f3fea5f3f6a4022c7780b81d8bc423bd73d111f3
Ajaypatel <Ajaypatelap1504@gmail.com>
2025-09-05 14:28:42 +0530

    1020. Number of Enclaves using BFS

---
+++ Graph/NumberOfEnclaves.java
@@ -0,0 +1,108 @@
+package Graph;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class NumberOfEnclaves {
+    /**
+     * 1020. Number of Enclaves
+     * Medium
+     * Topics
+     * premium lock icon
+     * Companies
+     * Hint
+     * You are given an m x n binary matrix grid, where 0 represents a sea cell and 1 represents a land cell.
+     *
+     * A move consists of walking from one land cell to another adjacent (4-directionally) land cell or walking off the boundary of the grid.
+     *
+     * Return the number of land cells in grid for which we cannot walk off the boundary of the grid in any number of moves.
+     *
+     *
+     *
+     * Example 1:
+     *
+     *
+     * Input: grid = [[0,0,0,0],[1,0,1,0],[0,1,1,0],[0,0,0,0]]
+     * Output: 3
+     * Explanation: There are three 1s that are enclosed by 0s, and one 1 that is not enclosed because its on the boundary.
+     * Example 2:
+     *
+     *
+     * Input: grid = [[0,1,1,0],[0,0,1,0],[0,0,1,0],[0,0,0,0]]
+     * Output: 0
+     * Explanation: All 1s are either on the boundary or can reach the boundary.
+     *
+     *
+     * Constraints:
+     *
+     * m == grid.length
+     * n == grid[i].length
+     * 1 <= m, n <= 500
+     * grid[i][j] is either 0 or 1.
+     * */
+    public static void main(String[] args) {
+        int[][] grid = {{0,0,0,0},{1,0,1,0},{0,1,1,0},{0,0,0,0}};
+        System.out.println(numEnclaves(grid));
+    }
+
+    static class Pair {
+        int first;
+        int second;
+        public Pair(int first, int second) {
+            this.first = first;
+            this.second = second;
+        }
+    }
+
+    //Solution 1: BFS
+    //Time Complexity: O(n*m) + O(n*m*4) ~ O(n*m)
+    //Space Complexity: O(n*m) + O(n*m) + O(n*m) ~ O(n*m)
+    public static int numEnclaves(int[][] grid) {
+        Queue<Pair> q = new LinkedList<Pair>();
+        int n = grid.length;
+        int m = grid[0].length;
+        int vis[][] = new int[n][m];
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                // first row, first col, last row, last col
+                if(i == 0 || j == 0 || i == n-1 || j == m-1) {
+                    if(grid[i][j] == 1) {
+                        q.add(new Pair(i, j));
+                        vis[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        int delRow[] = {-1, 0, 1, 0};
+        int delCol[] = {0, 1, 0, -1};
+
+        while(!q.isEmpty()) {
+            int row = q.peek().first;
+            int col = q.peek().second;
+            q.remove();
+
+            // traverses all 4 directions
+            for(int i = 0; i < 4; i++) {
+                int nrow = row + delRow[i];
+                int ncol = col + delCol[i];
+
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m
+                        && vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1) {
+                    q.add(new Pair(nrow, ncol));
+                    vis[nrow][ncol] = 1;
+                }
+            }
+        }
+
+        int cnt = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] == 1 && vis[i][j] == 0) cnt++;
+            }
+        }
+
+        return cnt;
+    }
+}
